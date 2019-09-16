aaacd99a6bccb430ac86a25f5564e5dec5e24829
George Kiknadze <g.kiknadze@hotmail.com>
2019-09-16 18:18:11 +0400

    tabs to spaces

--- app.js
+++ app.js
@@ -4,25 +4,25 @@ const { exec } = require('child_process');
 const argv = require('minimist')(process.argv.slice(2));
 
 if (argv.list) {
-	listSshKeys();
+  listSshKeys();
 } else {
-	console.log(`
-	list - List all existing ssh keys 🔑
-	generate - Generate new Ed25519 ssh key
-	`);
+  console.log(`
+  list - List all existing ssh keys 🔑
+  generate - Generate new Ed25519 ssh key
+  `);
 }
 
 
 const listSshKeys = () => exec(
-	`for key in ~/.ssh/id_*; do ssh-keygen -l -f "$\{key}"; done | uniq`,
-	(err, stdout, stderr) => {
-		if (err) {
-			console.log(err);
-			return;
-		} else if (stderr) {
-			console.log(stderr);
-			return;
-		}
+  `for key in ~/.ssh/id_*; do ssh-keygen -l -f "$\{key}"; done | uniq`,
+  (err, stdout, stderr) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else if (stderr) {
+      console.log(stderr);
+      return;
+    }
 
-		console.log(`stdout: ${stdout}`);
-	});
+    console.log(`stdout: ${stdout}`);
+  });
