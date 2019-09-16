493d89558d96d18f3aa73900d645df0b8bb9010a
George Kiknadze <g.kiknadze@hotmail.com>
2019-09-16 18:31:57 +0400

    add functionality to generate new ed25519 ssh keys

--- app.js
+++ app.js
@@ -3,16 +3,6 @@
 const { exec } = require('child_process');
 const argv = require('minimist')(process.argv.slice(2));
 
-if (argv.list) {
-  listSshKeys();
-} else {
-  console.log(`
-  list - List all existing ssh keys 🔑
-  generate - Generate new Ed25519 ssh key
-  `);
-}
-
-
 const listSshKeys = () => exec(
   `for key in ~/.ssh/id_*; do ssh-keygen -l -f "$\{key}"; done | uniq`,
   (err, stdout, stderr) => {
@@ -26,3 +16,23 @@ const listSshKeys = () => exec(
 
     console.log(`stdout: ${stdout}`);
   });
+
+const generateNewSsh = (email, name='id_ed25519') => exec(
+  `ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/${name} -C "${email}"`
+  );
+
+
+if (argv.l) {
+  listSshKeys();
+} else if (argv.g) {
+  if (argv.email && argv.email.length > 0) {
+    generateNewSsh(argv.email, argv.filename);
+  } else {
+    console.log('Please provide email address')
+  }
+} else {
+  console.log(`
+  -l - List all existing ssh keys 🔑
+  -g - Generate new Ed25519 ssh key
+  `);
+}
